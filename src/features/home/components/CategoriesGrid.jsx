473b78302f20c60acee20f1ce241ddb4b11a3825
yaseenyy77 <yaseenyy77x@gmail.com>
2026-05-24 22:44:40 +0300

    update

--- src/features/home/components/CategoriesGrid.jsx
+++ src/features/home/components/CategoriesGrid.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../../../supabaseClient'; 
+import { supabase } from '../../../../supabaseClient'; 
 
 const CategoriesGrid = ({ brandId, brandName }) => {
   const [categories, setCategories] = useState([]);
