00e5f818ff2d3cbdfa2e1eb22d39c408725e879a
yaseenyy77 <yaseenyy77x@gmail.com>
2026-07-31 14:53:30 +0300

    update

--- src/features/home/components/FeaturedProducts.jsx
+++ src/features/home/components/FeaturedProducts.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../../supabaseClient';
+import { supabase } from '../../../../supabaseClient';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { Eye, ChevronLeft, ChevronRight } from 'lucide-react';
